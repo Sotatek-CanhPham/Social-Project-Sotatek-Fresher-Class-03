72ddac4834ef9bd82481f6513db8709f67a08d24
Pham Huy Canh <canh.pham@sotatek.com>
2023-01-18 11:51:22 +0700

    Add NewPost

--- react-frontend/src/components/CreatePost/NewPost/index.tsx
+++ react-frontend/src/components/CreatePost/NewPost/index.tsx
@@ -1,10 +1,119 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {
+  faEarthAsia,
+  faLock,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
 import React from "react";
+import ImageIcon from "../../../assets/images/create-post/image.svg";
+import VideoIcon from "../../../assets/images/create-post/video.svg";
+import CloseIcon from "../../../assets/images/edit-profile/close.svg";
+import DownArrowIcon from "../../../assets/images/edit-profile/down-arrow.svg";
+import AvatarUser from "../../../assets/images/header/avatar.png";
 import styles from "./NewPost.module.scss";
 
-type Props = {};
+type Props = {
+  onClose: () => void;
+};
 
-const NewPost = (props: Props) => {
-  return <div className={styles["new-post"]}>NewPost</div>;
+const NewPost = ({ onClose }: Props) => {
+  const [status, setStatus] = React.useState("Public");
+  const [faIcon, setFaIcon] = React.useState<IconProp>(faEarthAsia);
+  const [isShowDropdown, setIsShowDropdown] = React.useState(false);
+
+  const show = (status: string, faIcon: IconProp) => () => {
+    setStatus(status);
+    setFaIcon(faIcon);
+  };
+
+  const showDropdown = (e: any) => {
+    setIsShowDropdown(!isShowDropdown);
+  };
+  return (
+    <div className={styles["new-post"]}>
+      <div className={styles["new-post__container"]}>
+        <h2>New Post</h2>
+        <img
+          alt=""
+          src={CloseIcon}
+          className={styles["close-btn"]}
+          onClick={onClose}
+        />
+        <div className={styles["new-post__container__header"]}>
+          <img alt="" src={AvatarUser} />
+          <div className={styles["new-post__container__header__right"]}>
+            <div className={styles["fullname"]}>Pham Huy Canh</div>
+            <div className={styles["post-status"]} onClick={showDropdown}>
+              <FontAwesomeIcon icon={faIcon} className={styles["fa-icon"]} />
+              <input
+                type="text"
+                className={classNames(
+                  styles["post-status-input"],
+                  "input-value"
+                )}
+                value={status || "Public"}
+                readOnly
+              />
+
+              <img
+                alt=""
+                src={DownArrowIcon}
+                className={styles["down-arrow"]}
+              />
+              {isShowDropdown && (
+                <div className={styles["post-status-option"]}>
+                  <div
+                    className={styles["post-status-option-item"]}
+                    onClick={show("Public", faEarthAsia)}
+                  >
+                    <FontAwesomeIcon icon={faEarthAsia} />
+                    <p>Public</p>
+                  </div>
+                  <hr />
+
+                  <div
+                    className={styles["post-status-option-item"]}
+                    onClick={show("Friend", faUserGroup)}
+                  >
+                    <FontAwesomeIcon icon={faUserGroup} />
+                    <p>Friend</p>
+                  </div>
+
+                  <hr />
+                  <div
+                    className={styles["post-status-option-item"]}
+                    onClick={show("Only me", faLock)}
+                  >
+                    <FontAwesomeIcon icon={faLock} />
+                    <p>Only me</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className={styles["new-post__container__content"]}>
+          <textarea placeholder="What's on your mind?" />
+        </div>
+        <div className={styles["new-post__container__media-option"]}>
+          <p>Add to your post</p>
+          <ul>
+            <li>
+              <img src={ImageIcon} alt="" />
+              <p>Image</p>
+            </li>
+            <li>
+              <img src={VideoIcon} alt="" />
+              <p>Video</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles["new-post__container__post-submit"]}>Post</div>
+      </div>
+    </div>
+  );
 };
 
 export default NewPost;
--- react-frontend/src/components/CreatePost/index.tsx
+++ react-frontend/src/components/CreatePost/index.tsx
@@ -1,29 +1,22 @@
-import React from "react";
-import styles from "./CreatePost.module.scss";
-import AvatarUser from "../../assets/images/header/avatar.png";
+import AttachmentIcon from "../../assets/images/create-post/attachment.svg";
+import EmojiIcon from "../../assets/images/create-post/emoji.svg";
+import HashtagIcon from "../../assets/images/create-post/hashtag.svg";
 import ImageIcon from "../../assets/images/create-post/image.svg";
 import VideoIcon from "../../assets/images/create-post/video.svg";
-import AttachmentIcon from "../../assets/images/create-post/attachment.svg";
-import HashtagIcon from "../../assets/images/create-post/hashtag.svg";
-import EmojiIcon from "../../assets/images/create-post/emoji.svg";
-import NewPost from "./NewPost";
+import AvatarUser from "../../assets/images/header/avatar.png";
+import styles from "./CreatePost.module.scss";
 
-type Props = {};
+type Props = {
+  onNewPostClick: () => void;
+};
 
-const CreatePost = (props: Props) => {
-  const handleClickInput = () => {
-    const newPost = document.querySelector(
-      `.${styles["create-post__container__new-post"]}`
-    );
-    newPost?.classList.toggle(styles["show-new-post"]);
-  };
-
+const CreatePost = ({ onNewPostClick }: Props) => {
   return (
     <div className={styles["create-post"]}>
       <div className={styles["create-post__container"]}>
         <div className={styles["create-post__container__post-writing"]}>
           <img src={AvatarUser} alt="" />
-          <div className={styles["post-input"]} onClick={handleClickInput}>
+          <div className={styles["post-input"]} onClick={onNewPostClick}>
             <p>Share something...</p>
             <img src={EmojiIcon} alt="" />
           </div>
@@ -48,9 +41,6 @@ const CreatePost = (props: Props) => {
               <p>Hashtag</p>
             </li>
           </ul>
-        </div>
-        <div className={styles["create-post__container__new-post"]}>
-          <NewPost />
         </div>
       </div>
     </div>
